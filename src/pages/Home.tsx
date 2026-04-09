c9f42d5e80ea74924b68132e057887d3b8e68f87
webierproductionenv <webierwebdev@gmail.com>
2026-04-09 19:01:18 +0530

    Verbiage change

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -249,9 +249,10 @@ const Home: React.FC = () => {
             </h2>
             <div className="w-24 h-[1px] bg-secondary mx-auto mb-10"></div>
             <p className="text-stone-500 text-lg md:text-xl leading-relaxed mb-12 w-full mx-auto px-4">
-              At EastPointe, the land and environment mean everything to us. Our
-              cabins are intentionally built using reclaimed and recycled wood
-              and materials. Because of this, you may notice that nothing is
+              Welcome to EastPointe At EastPointe, the land, the environment,
+              and our faith in GOD mean everything to us. Our cabins are
+              intentionally built using reclaimed and recycled wood and
+              materials. Because of this, you may notice that nothing is
               perfectly uniform — and that’s part of the beauty. Each cabin
               carries its own character and story, built from materials given a
               second life instead of being sent to a landfill. This rustic style
